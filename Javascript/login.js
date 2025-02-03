9f8b13923c6578082df30019a394eb6641d707f3
vicerhatech <victorchideibere@gmail.com>
2025-02-03 07:25:40 +0100

    create a sign up and login page for a freelance app

---
+++ Javascript/login.js
@@ -0,0 +1,8 @@
+document.querySelector(".toggle-password").addEventListener("click", function() {
+    const passwordInput = document.getElementById("password");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+});
